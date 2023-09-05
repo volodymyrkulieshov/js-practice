24fa2f1419bb01e3c4354e11bd165b88745bd7c1
Volodymyr <vovkul@gmail.com>
2023-09-05 12:48:14 +0200

    add_practice_Module4_2

--- js/script_Module4_2.js
+++ js/script_Module4_2.js
@@ -41,40 +41,163 @@
 // Task-3
 // Напишіть функцію, яка використовує метод find, щоб знайти книжку за її назвою (title).
 // Якщо книгу не знайдено повертається рядок 'Not found'.
-const books = [
-  {
-    title: "JavaScript: The Good Parts",
-    author: "Douglas Crockford",
-    year: 2008,
-  },
-  {
-    title: "Clean Code: A Handbook of Agile Software Craftsmanship",
-    author: "Robert C. Martin",
-    year: 2008,
-  },
-  {
-    title: "The Pragmatic Programmer: Your Journey to Mastery",
-    author: "Andrew Hunt, David Thomas",
-    year: 1999,
-  },
-  {
-    title: "Design Patterns: Elements of Reusable Object-Oriented Software",
-    author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
-    year: 1994,
-  },
-  {
-    title: "Refactoring: Improving the Design of Existing Code",
-    author: "Martin Fowler",
-    year: 1999,
-  },
-];
+// const books = [
+//   {
+//     title: "JavaScript: The Good Parts",
+//     author: "Douglas Crockford",
+//     year: 2008,
+//   },
+//   {
+//     title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+//     author: "Robert C. Martin",
+//     year: 2008,
+//   },
+//   {
+//     title: "The Pragmatic Programmer: Your Journey to Mastery",
+//     author: "Andrew Hunt, David Thomas",
+//     year: 1999,
+//   },
+//   {
+//     title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+//     author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+//     year: 1994,
+//   },
+//   {
+//     title: "Refactoring: Improving the Design of Existing Code",
+//     author: "Martin Fowler",
+//     year: 1999,
+//   },
+// ];
 
-function getBook(arr, title) {}
+// function getBook(arr, title) {
+//   const book = arr.find(({ title: bookTitle }) => bookTitle === title);
+//   return book || "Not found";
+// }
 
-console.log(
-  getBook(
-    books,
-    "Design Patterns: Elements of Reusable Object-Oriented Software"
-  )
-);
-console.log(getBook(books, "qwerty"));
+// console.log(
+//   getBook(
+//     books,
+//     "Design Patterns: Elements of Reusable Object-Oriented Software"
+//   )
+// );
+// console.log(getBook(books, "qwerty"));
+
+// Task-4
+// Напишіть функцію, яка використовує метод reduce,
+// щоб обчислити загальну вартість всіх товарів у масиві,
+// яка розраховується як добуток ціни товару на його кількість,
+// а потім сумується з іншими товарами.
+//  Результат повинен бути загальною вартістю всіх товарів.
+
+// const products = [
+//   { id: 1, name: "T-shirt", price: 20, quantity: 3 }, // 20 * 3 = 60
+//   { id: 2, name: "Jeans", price: 50, quantity: 2 },
+//   { id: 3, name: "Sneakers", price: 80, quantity: 1 },
+//   { id: 4, name: "Hat", price: 15, quantity: 4 },
+//   { id: 5, name: "Socks", price: 5, quantity: 6 },
+// ];
+
+// function getTotal(arr) {
+//   // return arr.reduce((acc, product) => {
+//   //   const totalPrice = product.price * product.quantity;
+//   //   return acc + totalPrice;
+//   // }, 0);
+
+//   return arr.reduce((acc, { price, quantity }) => acc + price * quantity, 0);
+// }
+// console.log(getTotal(products));
+
+//  Task-5
+// Напишіть функцію, яка використовує метод sort, щоб відсортувати книжки за роком видання у спадаючому порядку.
+// Результат повинен бути відсортованим масивом книжок за роком видання.
+
+// const books = [
+//   {
+//     title: "JavaScript: The Good Parts",
+//     author: "Douglas Crockford",
+//     year: 2008,
+//   },
+//   {
+//     title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+//     author: "Robert C. Martin",
+//     year: 2008,
+//   },
+//   {
+//     title: "The Pragmatic Programmer: Your Journey to Mastery",
+//     author: "Andrew Hunt, David Thomas",
+//     year: 1999,
+//   },
+//   {
+//     title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+//     author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+//     year: 1994,
+//   },
+//   {
+//     title: "Refactoring: Improving the Design of Existing Code",
+//     author: "Martin Fowler",
+//     year: 1999,
+//   },
+// ];
+
+// function sortDesc(arr) {
+//   return [...arr].sort((current, next) => next.year - current.year);
+// }
+// console.log(sortDesc(books));
+
+// Task-6
+// Напишіть функцію, яка використовує перебираючі методи масиву map та filter,
+// щоб отримати масив назв продуктів, ціна яких менше 2 доларів та відсортуй їх за алфавітним порядком.
+// const products = [
+//   { id: 2, name: "Banana", price: 0.99 }, //
+//   { id: 1, name: "Apple", price: 1.99 }, //
+//   { id: 3, name: "Orange", price: 2.49 }, //
+//   { id: 4, name: "Grapes", price: 3.99 }, //
+// ];
+
+// function getProducts(arr) {
+//   const products = arr
+//     .filter(({ price }) => price < 5)
+//     .map(({ name }) => name)
+//     .sort((a, b) => a.localeCompare(b));
+
+//   return products.reduce(
+//     (acc, product, idx) => acc + `${idx + 1} - ${product} \n`,
+//     products.length ? "Product list:\n" : "Products not found \n"
+//   );
+// }
+// console.log(getProducts(products));
+
+// Task-7 HARD 😈
+
+// Є рядок в якому довільна кількість літер,
+// гарантовано в рядку немає пробілів та розділових знаків,
+// потрібно повернути об'єкт де кожна літера буде ключем,
+// а кількість разів яку вона дублюється буде значенням ключа
+
+// Результат на який очікуємо
+// const obj = {
+//   a: 3,
+//   b: 4,
+//   s: 3,
+//   // ...
+// };
+// const str = "absdabsrgbadgtdswwbetflg";
+// const obj = {};
+// for (let i = 0; i < str.length; i += 1) {
+//   if (obj.hasOwnProperty(str[i])) {
+//     obj[str[i]] += 1;
+//   } else {
+//     obj[str[i]] = 1;
+//   }
+// }
+// console.log(obj);
+
+// const obj = str.split("").reduce((acc, key) => {
+//   if (key in acc) {
+//     acc[key] += 1;
+//   } else {
+//     acc[key] = 1;
+//   }
+//   return acc;
+// }, {});
+// console.log(obj);
