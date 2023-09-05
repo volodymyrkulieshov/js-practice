f26f0ae9a93061cd958cf99897ff986d67698eb9
Volodymyr <vovkul@gmail.com>
2023-09-05 16:43:11 +0200

    add_changes

--- js/home-work_4.js
+++ js/home-work_4.js
@@ -192,6 +192,20 @@
 // }
 // console.log(newArray);
 
+// function changeEven(numbers, value) {
+//   const newArray = [];
+//   numbers.forEach((number) => {
+//     if (number % 2 === 0) {
+//       newArray.push(number + value);
+//     } else {
+//       newArray.push(number);
+//     }
+//   });
+//   return newArray;
+// }
+
+// changeEven([17, 24, 68, 31, 42], 100);
+
 // 14)-----------------------
 // const planets = ["Earth", "Mars", "Venus", "Jupiter"];
 // // Change code below this line
@@ -701,10 +715,79 @@
 // Change code above this line
 
 // 48)-------------------------
+// [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: [
+//       "Jacklyn Lucas",
+//       "Linda Chapman",
+//       "Adrian Cross",
+//       "Solomon Fokes",
+//     ],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//   },
+// ];
 
-// Change code below this line
 // const getTotalBalanceByGender = (users, gender) => {
-//     return [...users]
-//         .filter((user) => user.gender === gender)
-//         .reduce((total, user) => total + user.balance, 0);
+//   return [...users]
+//     .filter((user) => user.gender === gender)
+//     .reduce((total, user) => total + user.balance, 0);
 // };
