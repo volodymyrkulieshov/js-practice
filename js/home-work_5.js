0d8b27be79d6c4f40e42cceaf5d7fd7b88b2e33b
Volodymyr <vovkul@gmail.com>
2023-09-15 16:23:46 +0200

    Add_Module6_1

--- js/home-work_5.js
+++ js/home-work_5.js
@@ -168,7 +168,44 @@
 // }
 
 // 10)--------------------------------
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів.
+// Клас очікує тільки один аргумент - початковий масив товарів, який записується
+// у властивість items об'єкта, що створюється.
+// Оголоси наступні методи класу:
+// getItems() - повертає масив поточних товарів у властивості items об'єкта,
+// який викликає цей метод.
+// addItem(newItem) - приймає новий товар newItem і додає його в масив товарів
+// у властивості items об'єкта, який викликає цей метод.
+// removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву
+// товарів у властивості items об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій
+// послідовності, в якій твій код перевірятимуть тести.Будь ласка, нічого там
+// не змінюй.
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     return this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     const index = this.items.indexOf(itemToRemove);
+//     if (!!~index) {
+//       this.items.splice(index, 1);
+//     }
+//   }
+// }
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
+// VERSION_2__________________________________
 // class Storage {
 //     constructor(items) {
 //         this.items = items;
@@ -196,6 +233,46 @@
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 // 11)---------------------------
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок,
+// який записується у властивість value об'єкта, що створюється.
+// Оголоси наступні методи класу:
+// getValue() - повертає поточне значення властивості value.
+// padEnd(str) - отримує параметр str(рядок) і додає його в кінець значення
+// властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str (рядок) і додає його на
+// початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str (рядок) і додає його на початок і
+// в кінець значення властивості value об'єкта, який викликає цей метод.
+
+class StringBuilder {
+  constructor(initialValue) {
+    this.value = initialValue;
+  }
+  getValue() {
+    return this.value;
+  }
+  padEnd(str) {
+    this.value = this.value + str;
+  }
+  padStart(str) {
+    this.value = str + this.value;
+  }
+
+  padBoth(str) {
+    this.padEnd(str);
+    this.padStart(str);
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+// ___VERSION_2_________________________________-
 // class StringBuilder {
 //     constructor(initialValue) {
 //         this.value = initialValue;
